de1ac00ce0081d8349817c6f12c80f3ff2e757bd
Misso Marchewsky <missoxd@gmail.com>
2021-03-23 20:24:27 -0300

    fix: customer attr select on federal_tax_payer_id

---
+++ Model/Config/Source/CustomerAttribute.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright © Intelipost. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Intelipost\Push\Model\Config\Source;
+
+use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\CollectionFactory;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class CustomerAttribute implements OptionSourceInterface
+{
+    const CUSTOMER_EAV_ENTITY_TYPE_ID = 1;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(CollectionFactory $collectionFactory)
+    {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * @return array
+     */
+    public function toOptionArray(): array
+    {
+        $collection = $this->collectionFactory
+            ->create()
+            ->setEntityTypeFilter(CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER)
+            ->addOrder('frontend_label', 'ASC');
+
+        $result = [
+            [
+                'value' => '',
+                'label' => __('Select CPF attribute')
+            ]
+        ];
+
+        foreach ($collection as $item) {
+            $result[] = [
+                'value' => $item->getAttributeCode(),
+                'label' => $item->getFrontendLabel()
+            ];
+        }
+
+        return $result;
+    }
+}
